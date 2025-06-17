d2c4d926af8ddd91f70cf56884fe07cb99de8195
Gilsa Media <120129694+GilsaMedia@users.noreply.github.com>
2025-06-17 16:56:35 +0300

    working in a 23
    still need to understand why its not working (i only looked on this in like 4 am because of the missile alarms)

--- src/Problem_23.java
+++ src/Problem_23.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Problem_23 {
     public static long isAbundant(long n) {
         long dividers = 0;
@@ -13,11 +15,37 @@ public class Problem_23 {
     }
     public static void main(String[] args) {
         int until = 28123;
-        long[] arr = new long[until];
+        long[] arr = new long[6965]; // it started as until and then change it to this to save wasted space
+        int index = 0;
         for (long i = 1; i <= until; i++) {
             if (isAbundant(i) != 0) {
-                arr[(int)(i-1)] = isAbundant(i);
+                arr[index] = isAbundant(i);
+                index++;
             }
         }
+        long sum = 0;
+        boolean add = true;
+        int z = 0;
+        long tempSum;
+        for (int i = 1; i < until; i++) {
+            for (int j = 1; j < index; j++) {
+                if (j != z) {
+                    tempSum = arr[z] + arr[j];
+                } else {
+                    break;
+                }
+                if (i == tempSum) {
+                    add = false;
+                    break;
+                }
+            }
+            if (add) {
+                sum += i;
+            }
+            if (z < 6964) {
+                z++;
+            }
+        }
+        System.out.println(sum);
     }
 }
